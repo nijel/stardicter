0f4fbb53e91f60c8575846f770ad52701a22aa04
Michal Čihař <michal@cihar.com>
2007-10-07 04:26:16 +0000

    Support for generating all snapshots.

--- slovnik_tarball.sh
+++ slovnik_tarball.sh
@@ -17,12 +17,20 @@
 # this program; if not, write to the Free Software Foundation, Inc.,
 # 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
+set -e
+
 # URL where to download source files
 url='http://slovnik.zcu.cz/files/slovnik_data_utf8.txt.gz'
 dir="stardict-english-czech-`date +%Y%m%d`"
+dira="$dir-ascii"
+diran="$dir-ascii-notags"
+dirn="$dir-notags"
 
 rm -rf $dir
 mkdir $dir
+mkdir $dira
+mkdir $dirn
+mkdir $diran
 cd $dir
 wget -q $url
 if [ ! -f slovnik_data_utf8.txt.gz ] ; then
@@ -33,9 +41,33 @@ if ! gunzip slovnik_data_utf8.txt.gz ; then
     echo 'Fail unzip!'
     exit 2
 fi
+cp slovnik_data_utf8.txt ../$dira
+cp slovnik_data_utf8.txt ../$diran
+cp slovnik_data_utf8.txt ../$dirn
 python ../slovnik2stardict.py
 dictzip *.dict
 rm slovnik_data_utf8.txt
 cd ..
 tar cfj $dir.tar.bz2 $dir
 rm -rf $dir
+cd $dira
+python ../slovnik2stardict.py --ascii
+dictzip *.dict
+rm slovnik_data_utf8.txt
+cd ..
+tar cfj $dira.tar.bz2 $dira
+rm -rf $dira
+cd $diran
+python ../slovnik2stardict.py --ascii --notags
+dictzip *.dict
+rm slovnik_data_utf8.txt
+cd ..
+tar cfj $diran.tar.bz2 $diran
+rm -rf $diran
+cd $dirn
+python ../slovnik2stardict.py --notags
+dictzip *.dict
+rm slovnik_data_utf8.txt
+cd ..
+tar cfj $dirn.tar.bz2 $dirn
+rm -rf $dirn
