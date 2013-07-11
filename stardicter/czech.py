0936d4770556a07f84f8afce3f84cc85c0999ddf
Michal Čihař <mcihar@suse.cz>
2013-07-11 16:48:34 +0200

    Slovnik cizich slov

---
+++ stardicter/czech.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright © 2006 - 2013 Michal Čihař <michal@cihar.com>
+#
+# This file is part of Weblate <http://weblate.org/>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+from stardicter.base import StardictWriter
+import urllib
+import gzip
+import cStringIO
+
+URL = 'http://slovnik-cizich-slov.abz.cz/export.php'
+
+
+class CzechWriter(StardictWriter):
+    url = 'http://slovnik-cizich-slov.abz.cz/'
+    name = u'Slovník cizích slov'
+    source = 'czech'
+    target = 'cizi'
+    license = 'GNU GPL license'
+    bidirectional = False
+
+    def is_data_line(self, line):
+        '''
+        Checks whether this is line with timestamp.
+        '''
+        return not line.startswith('#      Date:')
+
+    def is_header_line(self, line):
+        return line[0] == '#'
+
+    def download(self):
+        '''
+        Downloads dictionary data.
+        '''
+        handle = urllib.urlopen(URL)
+        return handle.read().decode('iso-8859-2')
+
+    def get_name(self, forward=True):
+        if forward:
+            return u'GNU/FDL Česko-Anglický slovník'
+        else:
+            return u'GNU/FDL Anglicko-Český slovník'
