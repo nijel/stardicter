194694e1133b32f2b4500e875b5eca0c3851f55b
Michal Čihař <mcihar@suse.cz>
2013-09-10 13:56:59 +0200

    Add test for words parsing

---
+++ stardicter/test_word.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright © 2006 - 2013 Michal Čihař <michal@cihar.com>
+#
+# This file is part of Weblate <http://weblate.org/>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+import unittest
+from stardicter.word import Word
+
+
+class WordTest(unittest.TestCase):
+    '''
+    Word class testsing.
+    '''
+    def assertParses(self, line, expected):
+        '''
+        Test for word parsing.
+        '''
+        word = Word.from_slovnik(line)
+        self.assertEqual(word.word, expected)
+        return word
+
+    def test_parse(self):
+        '''
+        Testing various weird stuff in parser.
+        '''
+        word = self.assertParses(
+            'a\tb\ttype\tnote\tauthor',
+            'a'
+        )
+        self.assertEquals(word.translation, 'b')
+        self.assertEquals(word.wtype, 'type')
+        self.assertEquals(word.note, 'note')
+        self.assertEquals(word.author, 'author')
